72885ae4238dc3c290677d18bab177f42a994154
padmini v <padmini.v@perfios.com>
2021-09-06 12:09:06 +0530

    ADDED: Welcome message in main method

---
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.tictactoe;
+
+public class TicTacToeGame 
+{
+
+	public static void main(String[] args)
+	{
+		TicTacToeGame tictactoe=new TicTacToeGame();
+		System.out.println("***Welcome to Tic-Tac-Toe game***");
+		
+	}
+}
