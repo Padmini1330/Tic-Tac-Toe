2e544a40a7aff087340013b31fced69c76e55917
padmini v <padmini.v@perfios.com>
2021-09-06 12:21:55 +0530

    ADDED: getValue method to read Player's mark

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,5 +1,8 @@
 package com.bridgelabz.tictactoe;
 
+
+import java.util.Scanner;
+
 public class TicTacToeGame 
 {
 	public static char[] board=new char[10];
@@ -11,13 +14,31 @@ public class TicTacToeGame
 			board[index]=' ';
 		}
 	}
+	
+	public void getInput() 
+	{
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Choose X or O");
+		char playerMark=scanner.next().toUpperCase().charAt(0);
+		char computerMark;
+		if (playerMark=='X')
+		{
+			computerMark='O';
+		}
+		else
+			computerMark='X';
+		System.out.println("Player :" +playerMark+ " computer:" +computerMark);
+	}
+	
 	public static void main(String[] args) 
 	{
 		TicTacToeGame tictactoe=new TicTacToeGame();
 		System.out.println("**Welcome to Tic-tac-toe game**");
 		tictactoe.createBoard();
+		tictactoe.getInput();
 		
 		
 	}
+	
 
 }
