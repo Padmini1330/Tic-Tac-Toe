ce30d493d26d9690582cf43e01ea0233b40148f0
padmini v <padmini.v@perfios.com>
2021-09-06 22:06:27 +0530

    REFACTORED: Added ability for the user to continue playing tictactoe

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -13,6 +13,7 @@ public class TicTacToeGame
 	public int[] freeSpaces=new int[10];
 	public static int emptyIndex=0;
 	int win=0;
+	public int playerWin=0,computerWin=0;
 	
 	Scanner scanner=new Scanner(System.in);
 	public void createBoard()
@@ -82,7 +83,7 @@ public class TicTacToeGame
 	            
 		else
 		{
-			System.out.println("Game tie");
+			System.out.println("Game tie!!");
 			System.exit(0);
 		}
 		
@@ -150,8 +151,15 @@ public class TicTacToeGame
 		}
 		else
 		{
+			winStatus();
 			System.out.println("Game finished!");
-			
+			if(playerWin==computerWin)
+				System.out.println("Game tie!!");
+			if(playerWin>computerWin)
+				System.out.println("Player Wins and Computer loses!!");
+			else
+				System.out.println("Computer Wins and Player loses!!");
+			System.exit(0);
 		}
 				
 	}
@@ -170,6 +178,7 @@ public class TicTacToeGame
 			}
 			
 		}
+		System.out.println();
 		
 	}
 
@@ -196,6 +205,44 @@ public class TicTacToeGame
 		
 		return win;
 			
+	}
+	
+	public void winStatus()
+	{
+		int row=1,column=1;
+		if(board[1]==playerKey && board[5]==playerKey && board[9]==playerKey)
+			playerWin++;
+		else if(board[3]==playerKey && board[5]==playerKey && board[7]==playerKey)
+			playerWin++;
+		for(int index=1;index<=3;index++)
+		{
+			if(board[row]== playerKey && board[row+1]==playerKey && board[row+2]==playerKey) 
+				playerWin++;
+			row=4;				
+		}
+		for(int index=1;index<=3;index++)
+		{
+			if(board[column]==playerKey && board[column+3]==playerKey && board[column+6]==playerKey) 
+				playerWin++;
+			column=2;				
+		}
+		
+		if(board[1]==computerKey && board[5]==computerKey && board[9]==computerKey)
+			computerWin++;
+		else if(board[3]==computerKey && board[5]==computerKey && board[7]==computerKey)
+			computerWin++;
+		for(int index=1;index<=3;index++)
+		{
+			if(board[row]== computerKey && board[row+1]==computerKey && board[row+2]==computerKey) 
+				computerWin++;
+			row=4;				
+		}
+		for(int index=1;index<=3;index++)
+		{
+			if(board[column]==computerKey && board[column+3]==computerKey && board[column+6]==computerKey) 
+				computerWin++;
+			column=2;				
+		}
 	}
 
 	public int result(char key)
@@ -238,17 +285,26 @@ public class TicTacToeGame
 	public static void main(String[] args) 
 	{
 		TicTacToeGame tictactoe=new TicTacToeGame();
+		Scanner scanner=new Scanner(System.in);
 		System.out.println("**Welcome to Tic-tac-toe game**");
-		tictactoe.createBoard();
-		tictactoe.getInput();
-		tictactoe.checkToss();
-		if(emptyIndex!=1)
+		System.out.println("1.Play game 2.exit game");
+		int choice=scanner.nextInt();
+		switch(choice)
 		{
-			System.out.println("Game finished!");
+		case 1: tictactoe.createBoard();
+				tictactoe.getInput();
+				tictactoe.checkToss();
+				break;
+				
+		case 2: System.exit(0);
+		
+		default: break;
 		}
+		
 		
 			
 		
 	}
+	
 
 }
